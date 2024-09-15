4b3a2e42c09da357aba94002193c6f4df0418969
Jacobtest <jacob.nat.gray@gmail.com>
2024-09-15 15:53:02 -0700

    Added Memoization

--- fib.c
+++ fib.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
-int fib_iterative(int num)
+unsigned long long memo[95];
+
+unsigned long long fib_iterative_wrapper(int num)
 {
-   if (num == 1)
+   if (num == 1 || num == 2)
    {
-      return 0;
+      return memo[num];
    }
-   else if (num == 2)
+   if (memo[num] != 0)
    {
-      return 1;
+      return memo[num];
    }
 
-   int num_prev = 0;
-   int num_current = 1;
-   int num_next;
+   unsigned long long num_prev = 0;
+   unsigned long long num_current = 1;
+   unsigned long long num_next;
 
    for (int i = 2; i < num; i++)
    {
@@ -25,26 +27,51 @@ int fib_iterative(int num)
    return num_current;
 }
 
-int fib_recursive(int num)
+unsigned long long fib_recursive_wrapper(int num)
 {
-   if (num == 1)
+   if (num == 1 || num == 2)
    {
-      return 0;
+      return memo[num];
    }
-   else if (num == 2)
+   if (memo[num] != 0)
    {
-      return 1;
+      return memo[num];
    }
 
-   return fib_recursive(num - 1) + fib_recursive(num - 2);
+   memo[num] = fib_recursive_wrapper(num - 1) + fib_recursive_wrapper(num - 2);
+   return memo[num];
+}
+
+unsigned long long fib_i(int num)
+{
+   if (num > 2 && memo[num] != 0)
+   {
+      return memo[num];
+   }
+
+   unsigned long long result = fib_iterative_wrapper(num);
+   memo[num] = result;
+   return result;
+}
+
+unsigned long long fib_r(int num)
+{
+   if (num > 2 && memo[num] != 0)
+   {
+      return memo[num];
+   }
+
+   unsigned long long result = fib_recursive_wrapper(num);
+   memo[num] = result;
+   return result;
 }
 
 int main(int argc, char *argv[])
 {
    int num_arg;
    int num_file;
-   int num_total;
-   int num_final;
+   unsigned long long num_total;
+   unsigned long long num_final;
    char option = argv[2][0];
    FILE *file = fopen(argv[3], "r");
 
@@ -55,15 +82,18 @@ int main(int argc, char *argv[])
 
    num_total = num_arg + num_file;
 
+   memo[1] = 0;
+   memo[2] = 1;
+
    if (option == 'i')
    {
-      num_final = fib_iterative(num_total);
-      printf("%d\n", num_final);
+      num_final = fib_i(num_total);
+      printf("%llu\n", num_final);
    }
    else if (option == 'r')
    {
-      num_final = fib_recursive(num_total);
-      printf("%d\n", num_final);
+      num_final = fib_r(num_total);
+      printf("%llu\n", num_final);
    }
 
    return 0;
